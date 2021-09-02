4c49b8f8a3ed3480e0e197ceb6dbd409183c5640
Nicholas Shi <nicholas.shi01@gmail.com>
2021-09-02 00:30:55 -0400

    Added temporary router object

--- client/App.js
+++ client/App.js
@@ -2,12 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Router from './Router';
+
+// Loading Views
+import HomeView  from './pages/Home/index';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <React.Fragment>
+      <Router />
+      { /* Placeholder to load and test each page individually*/ }
+      <HomeView />
+    </React.Fragment>
   );
 }
 
