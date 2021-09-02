d9f017c398aa429199ce5e554679a92f91f18c89
Nicholas Shi <nicholas.shi01@gmail.com>
2021-09-02 00:30:01 -0400

    Added sample home page component

---
+++ client/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import TaskList from '../../components/TaskList/index';
+
+export default HomeView = (props) => {
+    return (
+        <View>
+            <Text>Placeholder Home View</Text>
+            <Text>Placeholder Home View</Text>
+            <Text>Placeholder Home View</Text>
+            <Text>Placeholder Home View</Text>
+            <TaskList />
+        </View>
+    )
+}
