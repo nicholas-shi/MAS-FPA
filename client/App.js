57e125ea26fdf0a970326a7510e717f1d9d1a47b
rashmiathavale <rashmi3434@gmail.com>
2021-09-08 15:37:04 -0400

    added router logic for sign in, home and taskview page

--- client/App.js
+++ client/App.js
@@ -1,18 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import Router from './Router';
-
-// Loading Views
-import HomeView  from './pages/Home/index';
+import Routes from './Router';
 
 export default function App() {
   return (
     <React.Fragment>
-      <Router />
-      { /* Placeholder to load and test each page individually*/ }
-      <HomeView />
+      <Routes />
     </React.Fragment>
   );
 }
