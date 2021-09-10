08db9d14918ad60b72cc1253f39dece04f356a57
rashmiathavale <rashmi3434@gmail.com>
2021-09-10 12:13:56 -0400

    added sign in front end functionality

--- client/Router.js
+++ client/Router.js
@@ -3,6 +3,7 @@ import { Router, Scene } from 'react-native-router-flux';
 import SignInView from './pages/Sign-In/index';
 import HomeView from './pages/Home/index';
 import TaskView from './pages/TaskView/index';
+import SignUpView from './pages/Sign-Up/index';
 
 const Routes = (props) => (
    <Router>
@@ -10,6 +11,7 @@ const Routes = (props) => (
          <Scene key = "signin" component = {SignInView} title = "Sign-In" initial = {true} />
          <Scene key = "home" component = {HomeView} title = "Home" />
          <Scene key = "taskview" component = {TaskView} title = "TaskView" />
+         <Scene key = "signup" component = {SignUpView} title = "Sign-Up" />
       </Scene>
    </Router>
 )
---
+++ client/pages/Sign-Up/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default TaskView = (props) => {
+    return (
+        <View>
+            <Text>Placeholder Sign Up View</Text>
+            <Text>Placeholder Sign Up View</Text>
+            <Text>Placeholder Sign Up View</Text>
+            <Text>Placeholder Sign Up View</Text>
+        </View>
+    )
+}
